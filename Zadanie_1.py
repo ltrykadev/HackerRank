c4b1d1d6d17ad8e2f2afae032dea480b07a89ffb
Łukasz <lukasz@unlocked.pl>
2021-12-30 14:14:49 +0100

    even/odd

--- Zadanie_1.py
+++ Zadanie_1.py
@@ -1,12 +1,13 @@
-#!/bin/python
-
 import math
 import os
 import random
 import re
 import sys
 
-
-
 if __name__ == '__main__':
-    n = int(raw_input().strip())
+    n = int(input().strip())
+
+if (n % 2) == 0:
+    print("Even")
+else:
+    print("Odd")
