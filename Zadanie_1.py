6fda7b6fb50ee0f6c2fed6cb7bddca0f245c7afc
Łukasz <lukasz@unlocked.pl>
2021-12-30 14:55:28 +0100

    działa :)

--- Zadanie_1.py
+++ Zadanie_1.py
@@ -7,7 +7,14 @@ import sys
 if __name__ == '__main__':
     n = int(input().strip())
 
-if (n % 2) == 0:
-    print("Even")
+if (n % 2) != 0:
+    print('Weird')
 else:
-    print("Odd")
+    if (n >= 2) and (n <= 5):
+        print('Not Weird')
+    elif (n >= 6) and (n <=20):
+        print('Weird')
+    else:
+        print("Not Weird")
+
+
