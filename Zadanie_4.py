2c4c2f4108f14e77534f53a85b7ceda1587ca884
Łukasz <lukasz@unlocked.pl>
2021-12-31 13:30:41 +0100

    dodane warunki

--- Zadanie_4.py
+++ Zadanie_4.py
@@ -1,3 +1,16 @@
-if __name__ == '__main__':
-    N = int(input())
+import re
 
+if __name__ == '__main__':
+    count = int(input())
+    number = input()
+
+if len(number) == 10:
+    print("YES")
+else:
+    print("No")
+
+match = re.search(r'^[789]', number)
+if match:
+    print("match!")
+else:
+    print("no match!")
