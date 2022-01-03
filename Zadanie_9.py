322120a68df35300fe10811f82127bae1d05257c
Łukasz Tryka <87600808+ltrykadev@users.noreply.github.com>
2022-01-03 15:17:40 +0100

    Create Zadanie_9.py
    print the number of times that the substring occurs

---
+++ Zadanie_9.py
@@ -0,0 +1,21 @@
+def count_substring(string, sub_string):
+    count = 0
+    fin = 0
+    pos = 0
+
+    while fin != 1:
+        new_pos = string.find(sub_string, pos, len(string))
+        if new_pos == -1:
+            fin = 1
+            return count
+        else:
+            count += 1
+            pos = new_pos + 1
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
